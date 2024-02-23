0ced019282b32d3b76c2e698921104a8933a423a
Ailton Martins Guimarães <ailton@workline.com.br>
2024-02-23 16:50:42 -0300

    Criando objeto de usuário

--- src/components/pages/Auth/Register.js
+++ src/components/pages/Auth/Register.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Input from '../../form/Input'
+import { Link } from "react-router-dom";
+import styles from '../../form/Form.module.css'
 
 function Register() {
+  const [user, setUser] = useState({})
 
   function handleChange(e) {
-    
+    setUser({...user, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    // enviar o usuário para o banco
+    console.log(user);
   }
 
   return (
-    <section>
+    <section className={styles.form_container}>
       <h1>Registrar</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           text='Nome'
           type='text'
@@ -47,6 +57,9 @@ function Register() {
         />
         <input type="submit" value="Cadastrar" />
       </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
     </section>
   )
 }
