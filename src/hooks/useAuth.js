33a18bcedef85f6b546a6652d8938611121e8dc3
Ailton Martins Guimarães <ailton@workline.com.br>
2024-02-25 14:54:32 -0300

    Função de login

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -41,6 +41,25 @@ export default function useAuth() {
 
   }
 
+  async function login(user) {
+
+    let msgText = 'Login realizado com sucesso!'
+    let msgType = 'success'
+    
+    try {
+      const data = await api.post('/users/login', user).then((response) => {
+        return response.data
+      })
+
+      await authUser(data)
+    } catch (error) {
+      msgText = error.response.data.message
+      msgType = 'error'
+    }
+
+    setFlashMessage(msgText, msgType)
+  }
+
   async function authUser(data) {
     setAutenticated(true)   
     localStorage.setItem('token', JSON.stringify(data.token))
@@ -60,6 +79,6 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType)
   }
 
-  return {authenticated, register, logout}
+  return {authenticated, register, logout, login}
 
 }
