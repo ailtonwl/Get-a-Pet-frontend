4cd482609d1ccd5716d8d3f1ac8b5097151d2c23
Ailton Martins Guimarães <ailton@workline.com.br>
2024-02-25 17:21:59 -0300

    Atualização do usuário

--- src/components/pages/User/Profile.js
+++ src/components/pages/User/Profile.js
@@ -5,9 +5,12 @@ import styles from './Profile.module.css'
 import formStyles from '../../form/Form.module.css'
 import Input from '../../form/Input'
 
+import useFlashMessage from '../../../hooks/useFlashMessage';
+
 function Profile() {
   const [user, setUser] = useState({})
   const [token] = useState(localStorage.getItem('token') || '')
+  const {setFlashMessage} = useFlashMessage()
 
   useEffect(() => {
 
@@ -22,11 +25,35 @@ function Profile() {
   }, [token])
 
   function onFileChange(e) {
-    
+    setUser({...user, [e.target.name]: e.target.files[0]})
   }
 
   function handleChange(e) {
-    
+    setUser({...user, [e.target.name]: e.target.value})
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    let msgType='success'
+
+    const formData = new FormData()
+
+    await Object.keys(user).forEach((key) => formData.append(key, user[key]))
+
+    const data = await api
+      .patch(`/users/edit/${user._id}`, formData, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then((response) => {
+      return response.data
+    }).catch((err) => {
+      msgType = 'error'
+      return err.response.data
+    })
+
+    setFlashMessage(data.message, msgType)
   }
 
   return (
@@ -35,7 +62,7 @@ function Profile() {
         <h1>Perfil</h1>
         <p>Preview Imagem</p>
       </div>
-      <form className={formStyles.form_container}>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
         <Input 
           text="Imagem"
           type="file"
