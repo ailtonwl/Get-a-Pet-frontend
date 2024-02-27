cfe94aa3f0feccf9a72b0eab116ef19dc3f320b7
Ailton Martins Guimarães <ailton@workline.com.br>
2024-02-27 20:30:52 -0300

    Atualizando o Pet

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Login from './components/pages/Auth/Login'
 import Register from './components/pages/Auth/Register'
 import Home from './components/pages/Home'
 import Profile from './components/pages/User/Profile'
+import MyPets from './components/pages/Pet/MyPets';
+import AddPet from './components/pages/Pet/AddPet';
+import EditPet from './components/pages/Pet/EditPet';
 
 /* context */
 import { UserProvider } from "./context/UserContext";
@@ -26,6 +29,9 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/user/profile" element={<Profile />} />
+            <Route path="/pet/mypets" element={<MyPets />} />
+            <Route path="/pet/add" element={<AddPet />} />
+            <Route path="/pet/edit/:id" element={<EditPet />} />
             <Route path="/" element={<Home />} />
           </Routes>
           </Container>
